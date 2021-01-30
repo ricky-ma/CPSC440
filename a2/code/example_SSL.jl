f707608a36c254dc3bdf1653f56a052aac937481
ricky-ma <mr.rickyma@gmail.com>
2021-01-30 15:40:57 -0800

    Init commit

---
+++ a2/code/example_SSL.jl
@@ -0,0 +1,35 @@
+
+using JLD, Statistics, Printf
+
+# Load X and y variable
+data = load("SSL.jld")
+(X,y,Xtest,ytest,Xbar) = (data["X"],data["y"],data["Xtest"],data["ytest"],data["Xbar"])
+
+# Fit a KNN classifier
+k = 1
+include("knn.jl")
+model = knn(X,y,k)
+
+# Evaluate training error
+yhat = model.predict(X)
+trainError = mean(yhat .!= y)
+@printf("Train Error with %d-nearest neighbours: %.3f\n",k,trainError)
+
+# Evaluate test error
+yhat = model.predict(Xtest)
+testError = mean(yhat .!= ytest)
+@printf("Test Error with %d-nearest neighbours: %.3f\n",k,testError)
+
+include("gda.jl")
+model = gda(X,y)
+
+# Evaluate training error
+yhat = model.predict(X)
+trainError = mean(yhat .!= y)
+@printf("Train Error with GDA: %.3f\n",trainError)
+
+# Evaluate test error
+yhat = model.predict(Xtest)
+testError = mean(yhat .!= ytest)
+@printf("Test Error with GDA: %.3f\n",testError)
+
