c4223f13ef29fca0467f8ba473e641e4fdf9b08a
ricky-ma <mr.rickyma@gmail.com>
2021-02-06 17:24:48 -0800

    Fixed bugs - GDA test error: 0.376

--- a2/code/gda.jl
+++ a2/code/gda.jl
@@ -2,39 +2,42 @@ using Statistics
 
 function gda(X, y)
     # Filter and density estimation to get mu and Sigma for each class
-    labels = 1:length(unique(y))
+    k = length(unique(y))
+    d = size(X)[2]
     X_with_labels = [y X]
-    mus = Array{Float64}(undef,0,12)
-    Sigmas = []
-    pis = []
-    for label in 1:length(labels)
-        # filter data by class label and remove label column
-        Xc = X_with_labels[X_with_labels[:,1] .== label, :][:, 1:end .!= 1]
-        pic = length(Xc)/length(X)
-        pis = push!(pis, pic)
+    mus = Array{Float64}(undef,k,d)
+    Sigmas = Array{Float64}(undef,k,d,d)
+    pis = Array{Float64}(undef,k)
+    globalMu = mean(X, dims=1)
+    for c in 1:k
+        # filter data by class label, remove label column, and center
+        Xc = X_with_labels[X_with_labels[:,1] .== c, :]
+        Xc = Xc[:, 1:end .!= 1] .- globalMu
+        pis[c] = length(Xc)/length(X)
         # mean vector containing mean of each feature, by label
         mu = mean(Xc, dims=1)
-        mus = [mus; mu]
+        mus[c,:] = mu
         # calculate Sigma for this class
-        # Sigma = sum((Xc .- mu)*(Xc .- mu)') ./ length(Xc)
-        Sigma = cov(Xc)
-        Sigmas = push!(Sigmas, Sigma)
+        # println(dot((Xc[c,:]' - mu),(Xc[c,:]' - mu)'))
+        # Sigma = (Xc[:,c] - mu)*(Xc[:,c] - mu)' ./ length(Xc)
+        # println(size(Sigma))
+        Sigmas[c,:,:] = cov(Xc)
     end
 
-    function calculate_pdf(mu, Sigma, pic, xi)
-        log_variance = log(Sigma)
-        deter = -0.5 * logdet(inv(Sigma))
-        expon = -0.5 * ((xi' .- mu)*inv(Sigma)*(xi' .- mu)')
+    function PDF(mu, Sigma, pic, xi)
+        deter = -0.5 * logdet(Sigma)
+        expon = -0.5 * ((xi - mu)'*inv(Sigma)*(xi - mu))
         p = log(pic) .+ expon .+ deter
         return p[1][1]
     end
 
     function predict(Xhat)
+        xhatMu = mean(Xhat, dims=1)
         yhat = Vector{Int32}()
         for xi in eachrow(Xhat)
             probabilities = Vector{Float64}()
-            for c in 1:length(labels)
-                p = calculate_pdf(mus[c], Sigmas[c], pis[c], xi)
+            for c in 1:k
+                p = PDF(mus[c,:], Sigmas[c,:,:], pis[c], xi - xhatMu')
                 append!(probabilities, p)
             end
             # Use PDF and responsibility to get categorical distribution
