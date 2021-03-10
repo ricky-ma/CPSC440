bbbf37ee3de201987c170c7619817974b810958c
ricky-ma <mr.rickyma@gmail.com>
2021-03-10 01:12:55 -0800

    Finished 1.1.3, 1.1.5, 1.1.6

--- a3/code/example_Markov.jl
+++ a3/code/example_Markov.jl
@@ -17,13 +17,28 @@ k = length(p1)
 include("markov.jl")
 
 estimate = sampleAncestral(p1,pt,50,10000)
+exact, mostLikelyStates = marginalCK(p1,pt,50)
 println()
 println("Univariate marginals at time 50 using a Monte Carlo estimate based on 10000 samples:")
 println(round.(estimate, digits=3))
-
-exact = marginalCK(p1,pt,50)
 println("Exact univariate marginals at time 50 using CK equations:")
 println(round.(exact, digits=3))
-
 println("Error between approximation and exact:")
 println(round.(abs.(estimate - exact), digits=3))
+println()
+println("State with highest marginal probability for each time j:")
+println(mostLikelyStates)
+println()
+
+# decoding = viterbiDecode(p1,pt,100)
+# println()
+
+pgrad = [0,0,1,0,0,0,0]
+exact, mostLikelyStates = marginalCK(pgrad,pt,50)
+println("p(x50=c|x1=3):")
+println(round.(exact, digits=3))
+println()
+
+println("p(x5=c|x10=6):")
+estimate = sampleRejection(p1,pt,10000,5,10,6)
+println(round.(estimate, digits=3))
--- a3/code/markov.jl
+++ a3/code/markov.jl
@@ -1,16 +1,16 @@
 include("misc.jl")
 
-function sampleAncestral(p1,pt,steps,numSamples)
+function sampleAncestral(p1,pt,d,numSamples)
     k = length(p1)
     samples = Array{Float64}(undef,k,numSamples)
     # generate samples for Monte Carlo estimation
     for s in 1:numSamples
-        # sample ancestrally
-        state = p1
-        for t in 1:steps
-            state = pt[sampleDiscrete(state),:]
+        # sample ancestrally for d timesteps
+        stateDist = p1
+        for t in 1:d
+            stateDist = pt[sampleDiscrete(stateDist),:]
         end
-        samples[:,s] = state
+        samples[:,s] = stateDist
     end
     # Monte Carlo estimation
     estimate = Array{Float64}(undef,k)
@@ -19,20 +19,83 @@ function sampleAncestral(p1,pt,steps,numSamples)
         estimate[j] = sum(samples[j,:])
     end
     # normalize and return estimate
-    estimate = estimate ./ numSamples
-    return estimate
+    return estimate ./ numSamples
 end
 
 
-function marginalCK(p1,pt,steps)
+function sampleRejection(p1,pt,numSamples,queryTime,endTime,endState)
+    # Monte Carlo estimation via rejection sampling:
+    # estimate frequency of first event in samples consistent with second event
+    frequencies = [0,0,0,0,0,0,0]
+    acceptedSamples = 0
+    # generate samples for Monte Carlo estimation
+    for s in 1:numSamples
+        # flags for rejection sampling
+        accept = false
+        acceptState = 0
+        # sample ancestrally until endTime
+        stateDist = p1
+        for t in 1:endTime
+            state = sampleDiscrete(stateDist)
+            stateDist = pt[state,:]
+            # if t=queryTime, keep track of state
+            if t == queryTime
+                acceptState = state
+            end
+            # if t=endTime and we are at desired endState, accept sample
+            if t == endTime && state == endState
+                accept = true
+                acceptedSamples += 1
+            end
+        end
+        if accept
+            frequencies[acceptState] += 1
+        end
+    end
+    conditionalProbabilities = frequencies ./ acceptedSamples
+    return conditionalProbabilities
+end
+
+
+function marginalCK(p1,pt,d)
+    mostLikelyStates = Array{Int32}(undef,d)
     pd = p1
-    for t in 1:steps-1
+    for t in 1:d-1
+        val,ind = findmax(pd)
+        mostLikelyStates[t] = ind
         pd = pt' * pd
     end
-    return pd
+    val,ind = findmax(pd)
+    mostLikelyStates[d] = ind
+    return pd, mostLikelyStates
 end
 
 
-function viterbiDecode(p1,pt,steps)
+function viterbiDecode(p1,pt,d)
+    k = length(p1)
+    indices = Array{Int32}(undef,d,k)
+    optimalDecoding = Array{Int32}(undef,d-1)
 
+    # forward pass: store probabilities for each timestep
+    bestCase = p1
+    for t in 2:d
+        # enumerate all possible transitions
+        possibleTransitions = bestCase' .* pt
+        # choose max for each state and store in bestCase
+        bestCase,inds = findmax(possibleTransitions; dims=2)
+
+        # TODO: fix indexing so that backtracking works correctly
+        # save argmax indices to indices
+        indices[t-1,:] = [i[2] for i in inds]
+    end
+
+    # backtracking: find most likely sequence from the end
+    _, highestP = findmax(bestCase)
+    highestP = highestP[1]
+    for t=reverse(1:d-1)
+        optimalDecoding[t] = highestP
+        highestP = indices[t,highestP]
+    end
+    println(optimalDecoding)
+    return optimalDecoding
 end
